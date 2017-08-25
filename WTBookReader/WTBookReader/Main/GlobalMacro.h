b67692ac7be718196b83218839c7b2266b9429a4
liangbei <buyixiaobei@163.com>
2017-08-25 17:31:58 +0800

    试图修改主色调

--- WTBookReader/WTBookReader/Main/GlobalMacro.h
+++ WTBookReader/WTBookReader/Main/GlobalMacro.h
@@ -12,7 +12,7 @@
 // 颜色相关
 #define RGBA(r,g,b,a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:a]
 #define MainColor_Light RGBA(255,209,9,1)
-
+//#define MainColor_Light RGBA(221,243,207,1)
 
 // 屏幕尺寸
 #define MainScreenWidth [UIScreen mainScreen].bounds.size.width
